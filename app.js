f8f354ce2272fc729e8230d25ccf36e3df8c4b2b
Cournflaeik <elien.degeetere@gmail.com>
2022-10-19 17:54:00 +0200

    add: message router

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const messagesRouter = require ('./routes/messages');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(/api/messages, messagesRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
