d8ab7db54fcbc8292783aec59510f7abe56b9174
Cournflaeik <elien.degeetere@gmail.com>
2022-12-05 23:08:43 +0100

    feat: add get by id route

--- routes/messages.js
+++ routes/messages.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const messagesController = require('../controllers/messages');
 
 router.get("/",  messagesController.getAll);
+router.get("/:id", messagesController.getById);
 router.post("/" , messagesController.create);
 
 module.exports = router;
